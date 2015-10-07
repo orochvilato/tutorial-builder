e6d0974fa1a21f27522b2ebe60e413b88898dca7
informatique@chilbp.fr <orochvilato@chilbp.fr>
2015-10-07 14:59:27 -0400

    Work on Parameter class

--- capture/parameters.py
+++ capture/parameters.py
@@ -16,14 +16,35 @@ class ParameterError(Error):
 class Parameters:
     def __init__(self):
         # id, name, desc, type, group, profile
-        self._params = {}
-    def define(self,id,desc="",type="string",group="default",profile="default",value=None):
-        if not id in self._params.keys():
-            self._params[id] = dict(desc=desc,type=type,group=group,profile=profile,value=value)
+        self._defs = {}
+        self._values = {'default':dict()}
+        self.profile = 'default'
+    def define(self,id,desc="",type="string",group="default",value=None):
+        if not id in self._defs.keys():
+            self._defs[id] = dict(desc=desc,type=type,group=group,value=value)
         else:
             raise ParameterError('Parameter already defined')
+    def createProfile(self,profile):
+        if not profile in self._params.keys():
+            self._params[profile] = dict()
+        else:
+            raise ParameterError('Profile already created')
+    def selectProfile(self,profile):
+        if profile in self._params.keys():
+            self.profile = profile
+        else:
+            raise ParameterError('Profile not found')
+            
     def __getattr__(self,id):
-        return self._params[id]['value']
-    def __setattr(self,id,value):
-        self._params[id]['value'] = value
+        if id in self._params[self.profile].keys():
+            return self._params[self.profile][id]['value']
+        elif id in self._params['default'].keys():
+            return self._params['default'][id]['value']
+        else:
+            raise ParameterError('Parameter not found')
 
+    def __setattr(self,id,value):
+        if id in self._params['default'].keys():
+            self._params[self.profile]['value'] = value
+        else:
+            raise ParameterError('Parameter not found')
