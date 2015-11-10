bf802d6414a12c2a3907b2d3f095423d408a3ac5
informatique@chilbp.fr <orochvilato@chilbp.fr>
2015-11-10 14:46:53 -0400

    data.js

---
+++ capture/data.js
@@ -0,0 +1,189 @@
+var sce={
+    "desc": "",
+    "image": {
+        "name": "snap/tuto/tuto-001.png"
+    },
+    "name": "tuto",
+    "steps": [
+        {
+            "action": "anchor",
+            "id": "a0",
+            "image": "snap/tuto/tuto-001.png",
+            "message": {
+                "content": "# Start of tuto",
+                "type": "markdown"
+            },
+            "title": "Step 0"
+        },
+        {
+            "action": "zoom",
+            "h": 866,
+            "mask": true,
+            "w": 1217,
+            "x": 43,
+            "y": 65
+        },
+        {
+            "action": "click",
+            "button": "Left",
+            "count": 1,
+            "image": "snap/tuto/tuto-002.png",
+            "x": 132,
+            "y": 104
+        },
+        {
+            "action": "loadImage",
+            "image": "snap/tuto/tuto-002.png"
+        },
+        {
+            "action": "anchor",
+            "id": "a1",
+            "image": "snap/tuto/tuto-002.png",
+            "title": "Step 1"
+        },
+        {
+            "action": "zoom",
+            "h": 866,
+            "mask": true,
+            "w": 1217,
+            "x": 43,
+            "y": 65
+        },
+        {
+            "action": "click",
+            "button": "Left",
+            "count": 1,
+            "image": "snap/tuto/tuto-003.png",
+            "x": 157,
+            "y": 338
+        },
+        {
+            "action": "loadImage",
+            "image": "snap/tuto/tuto-003.png"
+        },
+        {
+            "action": "anchor",
+            "id": "a2",
+            "image": "snap/tuto/tuto-004.png",
+            "title": "Step 2"
+        },
+        {
+            "action": "zoom",
+            "h": 456,
+            "mask": true,
+            "w": 536,
+            "x": 384,
+            "y": 296
+        },
+        {
+            "action": "click",
+            "button": "Left",
+            "count": 1,
+            "image": "snap/tuto/tuto-005.png",
+            "x": 502,
+            "y": 351
+        },
+        {
+            "action": "loadImage",
+            "image": "snap/tuto/tuto-005.png"
+        },
+        {
+            "action": "anchor",
+            "id": "a3",
+            "image": "snap/tuto/tuto-006.png",
+            "title": "Step 3"
+        },
+        {
+            "action": "zoom",
+            "h": 456,
+            "mask": true,
+            "w": 536,
+            "x": 384,
+            "y": 296
+        },
+        {
+            "action": "click",
+            "button": "Left",
+            "count": 1,
+            "image": "snap/tuto/tuto-007.png",
+            "x": 578,
+            "y": 345
+        },
+        {
+            "action": "loadImage",
+            "image": "snap/tuto/tuto-007.png"
+        },
+        {
+            "action": "anchor",
+            "id": "a4",
+            "image": "snap/tuto/tuto-007.png",
+            "title": "Step 4"
+        },
+        {
+            "action": "zoom",
+            "h": 456,
+            "mask": true,
+            "w": 536,
+            "x": 384,
+            "y": 296
+        },
+        {
+            "action": "click",
+            "button": "Left",
+            "count": 1,
+            "x": 711,
+            "y": 352
+        },
+        {
+            "action": "anchor",
+            "id": "a5",
+            "image": "snap/tuto/tuto-008.png",
+            "title": "Step 5"
+        },
+        {
+            "action": "zoom",
+            "h": 456,
+            "mask": true,
+            "w": 536,
+            "x": 384,
+            "y": 296
+        },
+        {
+            "action": "click",
+            "button": "Left",
+            "count": 1,
+            "image": "snap/tuto/tuto-009.png",
+            "x": 860,
+            "y": 726
+        },
+        {
+            "action": "loadImage",
+            "image": "snap/tuto/tuto-009.png"
+        },
+        {
+            "action": "zoom",
+            "h": -1,
+            "mask": true,
+            "w": -1,
+            "x": 0,
+            "y": 0
+        },
+        {
+            "action": "loadImage",
+            "image": "snap/tuto/tuto-010.png"
+        },
+        {
+            "action": "zoom",
+            "h": -1,
+            "mask": true,
+            "message": {
+                "content": "# End of tuto",
+                "type": "markdown"
+            },
+            "w": -1,
+            "x": 0,
+            "y": 0
+        }
+    ],
+    "title": "tuto"
+};
